7eb8f712d18515d4cb05308675fd8f0cda89e140
shreyas <shreyas.zanpure@gmail.com>
2015-10-07 23:05:02 +0100

    Added poller

--- HeathersPantry/Global.asax.cs
+++ HeathersPantry/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -19,6 +22,53 @@ namespace HeathersPantry
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            SetupRefreshJob();
+        }
+
+        private static void SetupRefreshJob()
+        {
+            //remove a previous job
+            Action remove = HttpContext.Current.Cache["HeathersPantryRefresh"] as Action;
+            if (remove is Action)
+            {
+                HttpContext.Current.Cache.Remove("HeathersPantryRefresh");
+                remove.EndInvoke(null);
+            }
+
+            //get the worker
+            Action work = () =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(60000);
+                    WebClient refresh = new WebClient();
+                    try
+                    {
+                        refresh.UploadString("http://heatherspantry.apphb.com/", string.Empty);
+                    }
+                    catch (Exception)
+                    {
+                        //snip...
+                    }
+                    finally
+                    {
+                        refresh.Dispose();
+                    }
+                }
+            };
+            work.BeginInvoke(null, null);
+
+            //add this job to the cache
+            HttpContext.Current.Cache.Add(
+                "HeathersPantryRefresh",
+                work,
+                null,
+                Cache.NoAbsoluteExpiration,
+                Cache.NoSlidingExpiration,
+                CacheItemPriority.Normal,
+                (s, o, r) => { SetupRefreshJob(); }
+                );
         }
     }
 }
